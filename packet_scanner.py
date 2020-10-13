91fceb5703b5479df38780892b68607828fe26cf
wildenberg84 <wildenberg.github@gmail.com>
2020-10-13 14:46:41 +0200

    packet scanner pseudo-code

---
+++ packet_scanner.py
@@ -0,0 +1,24 @@
+
+# Displays packet information and saves packets as binary data 
+
+# get all local IP adresses with corresponding (human-readable, if available) NIC
+# request user input -- which IP / NIC to use
+
+# create a raw socket and bind it to a random available port on chosen IP / NIC
+
+# include the IP headers
+
+# enable promiscuous mode to forward all packets received
+    
+# open child process
+    # for all eternity
+        # receive from socket
+        
+        # update packet number
+            
+        # display (optional?)
+        # save to file (optional?)
+            
+# wait for user input to exit
+# close socket
+# exit
