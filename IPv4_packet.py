f57bf7ce947e287bb2a70ef272694031927dc3d5
wildenberg84 <wildenberg.github@gmail.com>
2020-10-16 00:35:39 +0200

    Basic setup for IPv4 packet class.
    Added methods for getting and setting IPv4 packet header and payload
    data.

---
+++ IPv4_packet.py
@@ -0,0 +1,113 @@
+
+
+class IPv4_packet:
+    
+    def __init__(self):
+        pass
+    
+    def parse_packet(self, packet):
+        # parse binary data of IP packet
+        pass
+    
+    
+    def get_version(self):
+        pass
+    
+    def set_version(self, version):
+        pass
+    
+    def get_ihl(self):
+        pass
+    
+    def set_ihl(self, ihl):
+        pass
+    
+    def get_dscp(self):
+        pass
+    
+    def set_dscp(self, dscp):
+        pass
+    
+    def get_ecn(self):
+        pass
+    
+    def set_ecn(self, ecn):
+        pass
+    
+    def get_total_length(self):
+        pass
+    
+    def set_total_length(self, length):
+        pass
+    
+    def get_identification(self):
+        pass
+    
+    def set_identification(self, identification):
+        pass
+    
+    def get_flags(self):
+        pass
+    
+    def set_flags(self, flags):
+        pass
+    
+    def get_flag_df(self):
+        pass
+    
+    def set_flag_df(self, flag):
+        pass
+    
+    def get_flag_mf(self):
+        pass
+    
+    def set_flag_mf(self, flag):
+        pass
+    
+    def get_fragment_offset(self):
+        pass
+    
+    def set_fragment_offset(self, offset):
+        pass
+    
+    def get_time_to_live(self):
+        pass
+    
+    def set_time_to_live(self, ttl):
+        pass
+    
+    def get_protocol(self):
+        pass
+    
+    def set_protocol(self, protocol):
+        pass
+    
+    def get_header_checksum(self):
+        pass
+    
+    def set_header_checksum(self, checksum):
+        pass
+    
+    def get_source_Address(self):
+        pass
+    
+    def set_source_address(self, address):
+        pass
+    
+    def get_destination_address(self):
+        pass
+    
+    def set_destination_address(self, address):
+        pass
+    
+    def get_options(self):
+        pass
+    
+    def set_options(self):
+        pass
+    
+    def get_payload(self):
+        pass
+    
+    def set_payload(self, payload):
+        pass
