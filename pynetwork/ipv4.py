3dcca60ccfaf632dfd096183f94983016d8ac9ba
wildenberg84 <wildenberg.github@gmail.com>
2020-10-23 22:49:42 +0200

    Added documentation and minor implementation details.

--- pynetwork/ipv4.py
+++ pynetwork/ipv4.py
@@ -122,182 +122,466 @@ class IPv4Header:
     
     ##### Start of GETTERS / SETTERS for IP header fields #####
     def get_version(self):
-        """Return the version."""
+        """
+        Return the version.
+        
+        Returns
+        -------
+            int 
+            
+        """
         
         return self.header[0] >> 4
     
     def set_version(self, version):
-        """Set the version."""
+        """
+        Set the version.
+        
+        Parameters
+        ----------
+            version : int
+                Protocol version (0 - 15, inclusive)
+        
+        """
         
         self.header[0] = (version << 4) + self.get_ihl()
     
     def get_ihl(self):
-        """Return the Internet Header Length."""
+        """
+        Return the Internet Header Length.
+        
+        Returns
+        -------
+            int
+        
+        """
         
         return self.header[0] & 0b00001111
     
     def set_ihl(self, ihl):
-        """Set the Internet Header Length."""
+        """
+        Set the Internet Header Length.
+        
+        Parameters
+        ----------
+            ihl : int
+                Internet Header Length (0 - 15, inclusive)
+        """
         
         self.header[0] = (self.header[0] & 0b11110000) + ihl
     
     def get_dscp(self):
-        """Return the Differentiated Services Code Point (Type of Service)."""
+        """
+        Return the Differentiated Services Code Point (Type of Service).
+        
+        Returns
+        -------
+            int
+        
+        """
         
         return (self.header[1] & 0b11111100) >> 2
     
     def set_dscp(self, dscp):
-        """Set the Differentiated Services Code Point (Type of Service)."""
+        """
+        Set the Differentiated Services Code Point (Type of Service).
+        
+        Parameters
+        ----------
+            dscp : int
+                Differentiated Services Code Point (0 - 63, inclusive)
+        
+        """
         
         self.header[1] = (dscp << 2) + self.get_ecn()
     
     def get_ecn(self):
-        """Return the Explicit Congestion Notification"""
+        # TODO: Update to RFC 2481
+        """
+        Return the Explicit Congestion Notification.
+        
+        Returns
+        -------
+            int
+                00 - Not-ECT
+                01 - ECT(1)
+                10 - ECT(0)
+                11 - CE
+            
+        """
         
         return self.header[1] & 0b00000011
     
     def set_ecn(self, ecn):
-        """Set the Explicit Congestion Notification"""
+        # TODO: Update to RFC 2481
+        """
+        Set the Explicit Congestion Notification
+        
+        Parameters
+        ----------
+            ecn : int
+                Explicit Congestion Notification (0 - 3, inclusive)
+                
+                00 - Not-ECT
+                01 - ECT(1)
+                10 - ECT(0)
+                11 - CE
+                
+        """
         
         self.header[1] = (self.header[1] & 0b11111100) + ecn
     
     def get_ecn_capable_transport(self):
-        """Return the ECN Capable Transport flag."""
+        # TODO: Update to RFC 2481
+        """
+        Return the ECN Capable Transport flag.
+        
+        Returns
+        -------
+            bool
+                True if ECN Capable, False otherwise
+        
+        """
         
         # either ECT(0) or ECT(1) -- treated equal by routers
-        if (self.header[1] & 0b00000010) >> 1 == 1 or self.header[1] & 0b00000001 == 1:
-            return 1
-        else:
-            return 0
+        return (self.header[1] & 0b00000010) >> 1 or self.header[1] & 0b00000001
     
-    def set_ecn_capable_transport(self, ecn_capable):
-        """Set the ECN Capable Transport flag."""
+    def set_ecn_capable_transport(self, ecn_capable=True):
+        # TODO: Update to RFC 2481
+        """
+        Set the ECN Capable Transport flag.
         
-        # set one of the 2 bits, but not both
-        self.header[1] = (self.header[1] & 0b11111100) + ecn_capable
+        Parameters
+        ----------
+            ecn_capable : bool, optional
+                True if transport is ECN capable
+                
+        """
+        
+        if ecn_capable:
+            # first bit should be set by sender, second by router
+            self.header[1] = (self.header[1] & 0b11111100) + 2
+        else:
+            self.header[1] = (self.header[1] & 0b11111100)
     
     def get_ecn_congestion_experienced(self):
-        """Return ECN congestion excperienced."""
+        """
+        Return ECN congestion excperienced.
+        
+        Returns
+        -------
+            bool
+                True if congestion is experienced, False otherwise
+            
+        """
         
         # both bits need to be set to 1 to indicate congestion
-        if self.header[1] & 0b00000011 == 3:
-            return 1
-        else:
-            return 0
+        return self.header[1] & 0b00000011
     
-    def set_ecn_congestion_experienced(self, congestion_experienced):
-        """Set ECN congestion excperienced."""
+    def set_ecn_congestion_experienced(self, congestion_experienced=True):
+        """
+        Set ECN congestion excperienced.
+        
+        Parameters
+        ----------
+            congestion_experienced : bool, optional
+                True if congestion is experienced (default is True)
+                
+        """
         
         self.header[1] = (self.header[1] & 0b11111100) + congestion_experienced
 
     def get_total_length(self):
-        """Return the Total Length of the packet in bytes."""
+        """
+        Return the Total Length of the packet in bytes.
+        
+        Returns
+        -------
+            int
+        
+        """
         
         return int.from_bytes(self.header[2:4], 'big', signed = False)
     
     def set_total_length(self, length):
-        """Set the Total Length of the packet in bytes."""
+        """
+        Set the Total Length of the packet in bytes.
+        
+        Parameters
+        ----------
+            length : int
+                length of the packet in bytes (20 - 65535, inclusive)
+        
+        """
         
         self.header[2:4] = length.to_bytes(2, 'big')
     
     def get_identification(self):
-        """Return the Identification."""
+        """
+        Return the Identification.
+        
+        Returns
+        -------
+            int
+            
+        """
         
         return int.from_bytes(self.header[4:6], 'big', signed = False)
     
     def set_identification(self, identification):
-        """Set the Identification."""
+        """
+        Set the Identification.
+        
+        Parameters
+        ----------
+            identification : int
+                Identification of the packet (0 - 65535, inclusive)
+        
+        """
         
         self.header[4:6] = identification.to_bytes(2, 'big')
     
     def get_flags(self):
-        """Return all flags."""
+        """
+        Return all flags.
+        
+        Returns
+        -------
+            int
+            
+        """
         
         return (self.header[6] & 0b11100000) >> 5
     
     def set_flags(self, flags):
-        """Set all flags."""
+        """
+        Set all flags.
+        
+        Parameters
+        ----------
+            flags : int
+                Fragment(ation) flags (0 - 3)
+                
+                bit 0: Reserved; must be zero.
+                bit 1: Don't Fragment (DF)
+                bit 2: More Fragments (MF)
+        
+        """
         
         # first bit should not be set to 1
         self.header[6] = (flags << 5) + self.get_fragment_offset()
     
     def get_flag_df(self):
-        """Return the Don't Fragment flag."""
+        """
+        Return the Don't Fragment flag.
         
-        return (self.header[6] & 0b01000000) >> 6 
+        Returns
+        -------
+            bool
+                True if Don't Fragment is set, False otherwise
+                
+        """
+        
+        return (self.header[6] & 0b01000000) >> 6
     
-    def set_flag_df(self, flag):
-        """Set the Don't Fragment flag."""
+    def set_flag_df(self, is_set=True):
+        """
+        Set the Don't Fragment flag.
         
-        self.header[6] = (self.header[6] & 0b10111111) + (flag << 6)
+        Parameters
+        ----------
+            is_set : bool
+                True if Don't Fragment should be set, False otherwise
+        
+        """
+        
+        if is_set:
+            self.header[6] = (self.header[6] & 0b10111111) + (1 << 6)
+        else:
+            self.header[6] = (self.header[6] & 0b10111111)
     
     def get_flag_mf(self):
-        """Return the More Fragments flag."""
+        """
+        Return the More Fragments flag.
+        
+        Returns
+        -------
+            bool
+                True if More Fragments is set, False otherwise
+        
+        """
         
         return (self.header[6] & 0b00100000) >> 5
     
-    def set_flag_mf(self, flag):
-        """Set the More Fragments flag."""
+    def set_flag_mf(self, is_set):
+        """
+        Set the More Fragments flag.
         
-        self.header[6] = (self.header[6] & 0b11011111) + (flag << 5)
+        Parameters
+        ----------
+            is_set : bool
+                True if More Fragments should be set, False otherwise
+        
+        """
+        
+        if is_set:
+            self.header[6] = (self.header[6] & 0b11011111) + (1 << 5)
+        else:
+            self.header[6] = (self.header[6] & 0b11011111)
     
     def get_fragment_offset(self):
-        """Return the Fragment Offset."""
+        """
+        Return the Fragment Offset.
+        
+        Returns
+        -------
+            int
+        
+        """
         
         return int.from_bytes(self.header[6:8], 'big', signed = False) - (self.get_flags() << 13)
     
     def set_fragment_offset(self, offset):
-        """Set the Fragment Offset."""
+        """
+        Set the Fragment Offset.
+        
+        Parameters
+        ----------
+            offset : int
+                indicates the offset of the fragment in eight-byte blocks (0 - 8191, inclusive)
+                NOTE: 8191 * 8 + 20 = 65548 bytes, which would exceed the maximum IP packet length of 65535 bytes
+                
+        """
         
         self.header[6:8] = ((self.get_flags() << 13) + offset).to_bytes(2, 'big')
     
     def get_time_to_live(self):
-        """Return the Time To Live."""
+        """
+        Return the Time To Live.
+        
+        Returns
+        -------
+            int
+        
+        """
         
         return self.header[8]
     
     def set_time_to_live(self, ttl):
-        """Set the Time To Live."""
+        """
+        Set the Time To Live.
+        
+        Parameters
+        ----------
+            ttl : int
+                indicates the Time To Live (0 - 255, inclusive)
+                
+        """
         
         self.header[8] = ttl
     
     def get_protocol(self):
-        """Return the Protocol."""
+        """
+        Return the Protocol.
+        
+        Returns
+        -------
+            int
+            
+        """
         
         return self.header[9]
     
     def set_protocol(self, protocol):
-        """Set the Protocol."""
+        """
+        Set the Protocol.
+        
+        Parameters
+        ----------
+            protocol : int
+                protocol identifier as directed by RFC 790 (0 - 255, inclusive)
+        
+        """
         
         self.header[9] = protocol
     
     def get_header_checksum(self):
-        """Return the Header Checksum."""
+        """
+        Return the Header Checksum.
+        
+        Parameters
+        ----------
+            int
+            
+        """
         
         return int.from_bytes(self.header[10:12], 'big', signed = False)
     
     def set_header_checksum(self, checksum):
-        """Set the Header Checksum."""
+        """
+        Set the Header Checksum.
+        
+        Parameters
+        ----------
+            checksum : int
+                header checksum used for error checking (0 - 65535, inclusive)
+        """
         
         self.header[10:12] = checksum.to_bytes(2, 'big')
     
     def get_source_address(self):
-        """Return the Source Address."""
+        """
+        Return the Source Address.
         
-        return int.from_bytes(self.header[12:16], 'big', signed = False)
+        Returns
+        -------
+            tuple(int, int, int, int)
+            
+        """
+        
+        return (self.header[12], self.header[13], self.header[14], self.header[15]) 
     
     def set_source_address(self, address):
-        """Set the Source Address."""
+        """
+        Set the Source Address.
         
-        self.header[12:16] = address.to_bytes(4, 'big')
+        Parameters
+        ----------
+            address : tuple(int, int, int, int)
+                sender of the packet (0 - 255, inclusive, for each value)
+        
+        """
+        
+        self.header[12] = address[0]
+        self.header[13] = address[1]
+        self.header[14] = address[2]
+        self.header[15] = address[3]
     
     def get_destination_address(self):
-        """Return the Destination Address."""
+        """
+        Return the Destination Address.
         
-        return int.from_bytes(self.header[16:20], 'big', signed = False)
+        Returns
+        -------
+            tuple(int, int, int, int)
+            
+        """
+        
+        return (self.header[16], self.header[17], self.header[18], self.header[19]) 
     
     def set_destination_address(self, address):
-        """Set the Destination Address."""
+        """
+        Set the Destination Address.
         
-        self.header[16:20] = address.to_bytes(4, 'big')
+        Parameters
+        ----------
+            address : tuple(int, int, int, int)
+                receiver of the packet (0 - 255, inclusive, for each value)
+        
+        """
+        
+        self.header[16] = address[0]
+        self.header[17] = address[1]
+        self.header[18] = address[2]
+        self.header[19] = address[3]
     ##### End of GETTERS / SETTERS for IP header fields #####
